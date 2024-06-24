c1c68b6c37a8efaa2715f5772676e9f9ba3dd1c1
Vikranth Kumar <srisevisa@gmail.com>
2024-06-24 14:01:52 -0400

    added right click and left click to navigate between examples

--- src/app.js
+++ src/app.js
@@ -361,5 +361,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  // Right click for nextModel
+  document.addEventListener('contextmenu', (e) => {
+    e.preventDefault(); // Prevent the default context menu from appearing
+    if (!app.viewer) return;
+    app.nextModel();
+  });
+
+  // Left click for prevModel
+  document.addEventListener('click', (e) => {
+    if (!app.viewer) return;
+    app.prevModel();
+  });
+
   console.info('[glTF Viewer] Debugging data exported as `window.VIEWER`.');
 });
